18f36d57b3ab045ddfb253d812ab1789bdde8545
pedrofsn <pedrokra@gmail.com>
2017-05-27 15:44:35 -0300

    teste da aula

--- aula1/aula1/src/java/br/ufg/inf/Servidor1.java
+++ aula1/aula1/src/java/br/ufg/inf/Servidor1.java
@@ -8,6 +8,7 @@ package br.ufg.inf;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author aluno
  */
+ @WebServlet(name = "MinhaServlet3", urlPatterns = {"/oi", "/ola"}) 
 public class Servidor1 extends HttpServlet {
 
     /**
@@ -27,8 +29,7 @@ public class Servidor1 extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
@@ -38,7 +39,7 @@ public class Servidor1 extends HttpServlet {
             out.println("<title>Servlet Servidor1</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Servidor1 at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet Servidor1 at teste " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
