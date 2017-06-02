581c29cbf8c25b43113430328d44799ec9c463b9
pedrofsn <pedrokra@gmail.com>
2017-06-01 21:52:55 -0300

    projeto de exemplo

---
+++ aula1/meu_servlet/src/br/ufg/esp/Contato.java
@@ -0,0 +1,43 @@
+package br.ufg.esp;
+
+import java.util.Date;
+
+public class Contato {
+
+	private String nome;
+	private String endereco;
+	private String email;
+	private Date dataNascimento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+}
